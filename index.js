3bb9736077b81e4ea122645aa3e9cccb9efaefb0
Cooper Burden <cooperburden@Coopers-MacBook-Pro-2.local>
2024-11-20 11:05:00 -0700

    Initial commit with project files

---
+++ index.js
@@ -0,0 +1,40 @@
+let express = require("express");
+let app = express();
+let path = require("path");
+
+const port = 5500;
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Specify the 'views' folder
+
+// Middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Define the knex configuration to connect to the PostgreSQL database
+const knex = require('knex')({
+  client: 'pg', // PostgreSQL client
+  connection: {
+    host: 'localhost', // PostgreSQL server's host
+    user: 'postgres', // Your PostgreSQL username
+    password: 'password', // Your PostgreSQL password
+    database: 'Project403' // Your database name
+  }
+});
+
+// Main route to render data from the database
+app.get("/", (req, res) => {
+  knex.select('*')  // Select all data
+    .from('countries')  // From the 'countries' table
+    .orderBy('country')  // Order by the 'country' column (make sure this column exists)
+    .then(countries => {
+      res.render("index", { countries: countries });  // Pass the 'countries' data to the 'index' view
+    })
+    .catch(err => {
+      console.log(err);  // Log the error if something goes wrong
+      res.status(500).json({ err });  // Send a 500 response in case of an error
+    });
+});
+
+// Start the Express app on the specified port
+app.listen(port, () => console.log(`Express App has started and server is listening on port ${port}!`));
